e1f86b11de422b4b0a47922c2766b9ff474481c6
Shuyang Cao <shuyang.cao@outlook.com>
2026-06-16 20:03:33 +0800

    Fix import errors in test_moe_transform.py

--- test/prototype/moe_qat/test_moe_qat_transform.py
+++ test/prototype/moe_qat/test_moe_qat_transform.py
@@ -4,7 +4,7 @@ import pytest
 import torch
 
 from torchao.prototype.moe_qat import MoEQATConfig
-from torchao.prototype.moe_qat.tensor import (
+from torchao.prototype.moe_qat.wrapper_tensor import (
     FakeQuantizedWeightWrapperBaseTensor,
     Float8FakeQuantizedWeightWrapperTensor,
     MXFakeQuantizedWeightWrapperTensor,
